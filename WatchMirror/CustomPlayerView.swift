9e97ec24cea0baa264d0881938a756a7a43f8454
Anastasia Kravchenko <anastasia.kravchenko@sibers.com>
2023-01-24 18:42:26 +0600

    made it all work

--- WatchMirror/CustomPlayerView.swift
+++ WatchMirror/CustomPlayerView.swift
@@ -12,53 +12,75 @@ struct PlayerView: UIViewRepresentable {
 
     let player: AVPlayer
 
-    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PlayerView>) {
-    }
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PlayerView>) {}
 
     func makeUIView(context: Context) -> UIView {
         return PlayerUIView(player: player)
     }
 
-
 }
 
 struct PlayerContainerView : View {
+
     private let player: AVPlayer
+    private var playerControlsView: PlayerControlsView
+
     init(player: AVPlayer) {
         self.player = player
+        playerControlsView = PlayerControlsView(player: player)
     }
+    
     var body: some View {
         ZStack {
             PlayerView(player: player)
-            PlayerControlsView(player: player)
+            playerControlsView
         }
+    }
+
+    func didTap(_ point: CGPoint, parentSize: CGSize) {
+        playerControlsView.didTap(point, parentSize: parentSize)
     }
 }
 
-struct PlayerControlsView : View {
+struct PlayerControlsView: View {
 
-    @State var playerPaused = true
+    @ObservedObject private var viewModel = ViewModel()
+    private let buttonSize = CGSize(width: 100, height: 100)
+
     let player: AVPlayer
     var body: some View {
-        Button(action: {
-            buttonAction()
-        }) {
-            Image(systemName: playerPaused ? "play" : "pause")
-                .tint(.white)
-                .frame(width: 50, height: 50)
-                .background(RoundedRectangle(cornerRadius: 25).stroke(.white))
-        }
+            Button(action: {
+                buttonAction()
+            }) {
+                Image(systemName: viewModel.playerPaused ? "play" : "pause")
+                    .tint(.white)
+                    .frame(width: buttonSize.width, height: buttonSize.height)
+                    .background(RoundedRectangle(cornerRadius: buttonSize.width/2).stroke(.white))
+            }
+            .onReceive(viewModel.$playerPaused) { playerPaused in
+                if playerPaused { self.player.pause() }
+                else { self.player.play() }
+            }
+    }
 
+    func didTap(_ point: CGPoint, parentSize: CGSize) {
+            let buttonFrame = CGRect(x: (parentSize.width - buttonSize.width)/2,
+                                 y: (parentSize.height - buttonSize.height)/2,
+                                 width: buttonSize.width,
+                                 height: buttonSize.height)
+        if buttonFrame.contains(point) {
+            buttonAction()
+        }
     }
 
     func buttonAction() {
-        self.playerPaused.toggle()
-        if self.playerPaused {
-            self.player.pause()
-        }
-        else {
-            self.player.play()
-        }
+        viewModel.playerPaused.toggle()
+    }
+}
+
+extension PlayerControlsView {
+    @MainActor class ViewModel: ObservableObject {
+        @Published var playerPaused = true
     }
 }
 
